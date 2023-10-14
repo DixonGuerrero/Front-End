e13374176bfcc41ecd74e5fc336b4a56a1457c13
MadagascarFilms <dixonguerrero1204@gmail.com>
2023-10-13 21:58:30 -0500

    Primer update homepages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminEditPersonsComponent } from './screens/admin-edit-persons/admin-edit-persons.component';
+import { HomePageComponent } from './screens/home-page/home-page.component';
 
 
 const routes: Routes = [
-  { path: 'admin/edit/person', component:AdminEditPersonsComponent}
-  
+  { path: 'admin/edit/person', component:AdminEditPersonsComponent},
+  { path: 'homepage', component:HomePageComponent},
+  { path: '', component:HomePageComponent}
 ];
 
 @NgModule({
